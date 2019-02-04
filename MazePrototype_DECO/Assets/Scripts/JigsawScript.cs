3b7ed1fe1f9ae69e37031557cfe0b6d600ae22a8
oisintheginger <32101314+oisintheginger@users.noreply.github.com>
2019-02-04 20:23:02 +0000

    Second Commit

---
+++ MazePrototype_DECO/Assets/Scripts/JigsawScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JigsawScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] gridSquares;
+
+    bool[] gridCheck;
+
+    bool allTrue;
+    // Update is called once per frame
+    
+    void FixedUpdate()
+    {   
+       
+        if(AllSquareFill()==true)
+        {
+            Debug.Log("Win");
+        }
+            
+    }
+
+    private bool AllSquareFill()
+    {
+        for (int i = 0; i< gridSquares.Length; i++)
+        {
+            if(gridSquares[i].GetComponent<SquareScript>().detectionS == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ MazePrototype_DECO/Assets/Scripts/SquareScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareScript : MonoBehaviour
+{
+    public bool detectionS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        detectionS = false;
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag=="Square")
+        {
+            detectionS = true;
+        }
+
+        
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Square")
+        {
+            detectionS = false;
+        }
+    }
+}
